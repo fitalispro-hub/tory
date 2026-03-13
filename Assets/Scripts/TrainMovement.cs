000383ae22b093c51a424e0cd75057f05b761316
Dmitri-o <dimkap1204@gmail.com>
2026-03-14 01:45:36 +0200

    Animacja

---
+++ Assets/Scripts/TrainMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TrainMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float destroyX = 12f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x > destroyX)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
